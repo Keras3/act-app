65c55bec62d789c704592631fa7228fefc9f8cdc
Keras3 <adachi.takanori@comp.cs.ehime-u.ac.jp>
2019-04-22 22:25:41 +0900

    My Django app, first commit

---
+++ .act_rep/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/adachi.takanori/web_app/.act_rep/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ README.md
@@ -0,0 +1,2 @@
+act-rep-blog
+# web-app-blog
